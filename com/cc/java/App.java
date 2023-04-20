a25c0d3b39e0285e9680278fb319d4f46b7ebb25
ASK-DONE <knueppelalex@web.de>
2023-04-20 10:40:38 +0200

    6. save 20.04.23

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,19 +8,23 @@ public class App {
         // Cat -- Referenztyp
         // cat -- Referenzvariable
 
-        Cat cat1 = new Cat();  
-        output("Blick von aussen: "+ cat1);
-        output(cat1.tellYourAddress());
+        Cat cat1 = new Cat("Grizabella", "white", 29);  
+        // output("Blick von aussen: "+ cat1); (inst.Var. wird
+        // output(cat1.tellYourAddress());  - wird nicht mehr gebraucht)
 
         //Wertzuweißung
-        cat1.changeName("Grizabella");
-        cat1.changeFurColor( "white");
-        cat1.changeAge(29);
-
+        // cat1.changeName("Grizabella");
+        // cat1.changeFurColor( "white");
+        // cat1.changeAge(29);
+        
+        // cat1.furColor = "grey"; = Designschwäche */    //
+        // jeder der diese Adresse kennt kann diese Zeile ändern
+        // Behebung: Einträge der Klasse Cat auf privat setzen! 
+       
         //Ausgabe
-        output(cat1.tellYourName());
-        output(cat1.tellYourFurColor());
-        output(Integer.toString(cat1.tellYourAge()));
+        output(cat1.getName());
+        output(cat1.getFurColor());
+        output(Integer.toString(cat1.getAge()));
         
         // output(cat1.age);                  // geht nicht*/
         // output(String.valueOf(2));         // geht auch..
@@ -30,22 +34,22 @@ public class App {
         output("-------------------------");
 
 
-        Cat cat2 = new Cat();  
-        output("Blick von aussen: "+cat2);
-        output(cat2.tellYourAddress());
+        Cat cat2 = new Cat("Alonzo", "grey", 35);  
+        // output("Blick von aussen: "+cat2);
+        // output(cat2.tellYourAddress());
 
         // Alte Option ohne change..-funktion
         // cat2.name = "Alonzo";
         // cat2.furColor = "grey";
         // cat2.age = 35;
 
-        cat2.changeName("Alonzo");
-        cat2.changeFurColor( "grey");
-        cat2.changeAge(35);
+        // cat2.changeName("Alonzo");
+        // cat2.changeFurColor( "grey");
+        // cat2.changeAge(35);
 
-        output(cat2.tellYourName());
-        output(cat2.tellYourFurColor());
-        output(Integer.toString((cat2.tellYourAge())));
+        output(cat2.getName());
+        output(cat2.getFurColor());
+        output(Integer.toString((cat2.getAge())));
 
         
     }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,40 +2,82 @@ package com.cc.java;
 
 public class Cat {
 
-    public String name;
-    public String furColor; 
-    public int age; 
+    private String name;
+    private String furColor; 
+    private int age; 
+    
+    
+    // Custom-Constructor
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+
+        
+    }
 
 
-    // this -- Instanzvariable
-    public String tellYourAddress() {
-        return "Blick von innen: " + this;
+    public String getName() {
+        return name;
     }
 
-    public String tellYourName() {
-        return this.name;   
-    }
-    
-    public String tellYourFurColor() {
-        return this.furColor;
-    }
-    
-    public int tellYourAge() {
-        return this.age;
-    }
-    
-    // Change *//
-    public void changeName(String name) {
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void changeFurColor(String furColor) {
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+
+    public void setFurColor(String furColor) {
         this.furColor = furColor;
     }
 
-    public void changeAge(int age) {
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
         this.age = age;
     }
+
+
+
+    // this -- Instanzvariable (wird nach dem Constructor
+    // nicht mehr benötigt)
+    // public String tellYourAddress() {
+    //     return "Blick von innen: " + this;
+    // }
+
+//     public String tellYourName() {
+//         return this.name;   
+//     }
+    
+//     public String tellYourFurColor() {
+//         return this.furColor;
+//     }
+    
+//     public int tellYourAge() {
+//         return this.age;
+//     }
+    
+//     // Change *//
+//     public void changeName(String name) {
+//         this.name = name;
+//     }
+
+//     public void changeFurColor(String furColor) {
+//         this.furColor = furColor;
+//     }
+
+//     public void changeAge(int age) {
+//         this.age = age;
+//     }
 
 }
     
